006713507e8c7b33ed1bdb20c9dac60643e9a0bd
Erik Muttersbach <erik@muttersbach.net>
2014-11-11 08:26:01 +0100

    Modified benchmark execution script to run the benchmark n times

--- benchmark.rb
+++ benchmark.rb
@@ -19,6 +19,18 @@ def combine(params, options, &block)
   end
 end
 
+# Main: Parse arguments
+n = 1
+
+if ARGV.count == 0
+  print "Usage: benchmark.rb [-n COUNT] FILE ARG1 ARG2 ARG...\n"
+  exit 1
+end
+
+if ARGV[0] == '-n'
+  n = Integer(ARGV[1])
+  ARGV = ARGV[2..ARGV.count-1]
+end
 
 options = ARGV[1..ARGV.count-1].map do |arg|
   if arg[0] == '['
@@ -31,10 +43,13 @@ end
 File.open('benchmark_'+options.map{|o|o.join(',').gsub('/', '')}.join('_')+'.csv', 'w') do |f|
   combine [], options do |params|
     print "Running #{params.join(',')}\n"
-    f.write params.join(',')
-    out = `/usr/bin/time -f "%e" #{ARGV[0]} #{params.join(' ')} 2>&1`
-    time = out.split("\n").last
-    f.write ",#{time}\n"
+    
+    for i in 1..n do 
+      f.write params.join(',')
+      out = `#{ARGV[0]} #{params.join(' ')}`
+      time = out.split("\n").last
+      f.write ",#{time}\n"
+    end
   end
 end
 
